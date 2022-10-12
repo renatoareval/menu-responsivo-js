89abb6ef6536fc5c04e7f7b46e64c8af5a5e5552
Renato Areval <renattoareval@gmail.com>
2022-10-11 22:18:10 -0400

    menu responsivo

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,8 +1,21 @@
-let menu = document.querySelector("#menu");
+const menu = document.querySelector('#menu');
+
+const menuIcon = document.querySelector('#menu-icon')
 
 
 function showMenu(){
-
-  menu.classList.toggle("showMenu");
+  menu.classList.toggle('show')
+  changeMenuIcon()
 }
 
+menuIcon.addEventListener('click',showMenu)
+
+
+function changeMenuIcon(){
+  if(menuIcon.classList.contains('show')){
+    menuIcon.src = 'assets/imgs/close.svg'
+  }
+  else{
+    menuIcon.src = 'assets/imgs/menu-hamburguer.svg'
+  }
+}
